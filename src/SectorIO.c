fbea4a763d4eb903986e1e7e313b61a78d5550ac
purmirl <gmlrnjs1994@gmail.com>
2019-03-20 14:35:57 +0900

    Add comments in the SectorIO.c

--- src/SectorIO.c
+++ src/SectorIO.c
@@ -1,10 +1,24 @@
+/**
+ * 2019 EXT2 Study
+ * SectorIO.h
+ */
+
 #include "SectorIO.h"
+
 static FILE* image;
+
+/** 
+ * init_IO() : File open function.
+ */
 void init_IO(){
 	image = fopen("HDD.img","rb+");
 	if(image == NULL)
 		fprintf(stderr,"fopen failed...!\n");
 }
+
+/**
+ * Block_Read(void* buffer, int block_num, int block_count) : Read one block function.
+ */
 int Block_Read(void* buffer, int block_num, int block_count){
 	if(fseek(image, block_num * KU_EXT2_BLOCK_SIZE,SEEK_SET)<0){
 		fprintf(stderr,"fseek failed...!\n");
@@ -12,6 +26,10 @@ int Block_Read(void* buffer, int block_num, int block_count){
 	}
 	return fread(buffer, 1, KU_EXT2_BLOCK_SIZE* block_count, image) / KU_EXT2_BLOCK_SIZE;
 }
+
+/**
+ * Block_Write(void* buffer, int block_num, int block_count) : Write one block function.
+ */
 int Block_Write(void* buffer, int block_num,  int block_count){
 	if(fseek(image, block_num * KU_EXT2_BLOCK_SIZE,SEEK_SET)<0){
 			fprintf(stderr,"fseek failed...!\n");
@@ -19,6 +37,10 @@ int Block_Write(void* buffer, int block_num,  int block_count){
 	}
 	return fwrite(buffer, 1, KU_EXT2_BLOCK_SIZE* block_count, image) / KU_EXT2_BLOCK_SIZE;
 }
+
+/**
+ * close_IO() : File close function.
+ */
 int close_IO(){
 	return fclose(image);
 }
