2a2571e3728cc3814d5e00f9d49843abdd47e552
purmirl <gmlrnjs1994@gmail.com>
2019-03-20 14:28:21 +0900

    Add the comments in main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,19 @@
+/**
+ * 2019 EXT2 Study
+ * main.c
+ * ------------
+ *
+ * Ext2 Format
+ *  - Boot Block : First ext2's partition block.
+ *  - Boot Group 0 ~ N : Partition except boot block of ext2.
+ *  - Super Block : 1 Block. Information of the whole ext2 file system.
+ *  - Group Descriptor : n Blocks. Information of the whole block groups.
+ *  - Data Block Bitmap : 1 Block. State of the block using.
+ *  - Inode Bitmap : 1 Block. State of the Inode using.
+ *  - Inode Table : n Blocks. Information of the whole Inode.
+ *  - Data Blocks : n Blocks. Data blocks.
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
@@ -14,13 +30,20 @@ int main(){
 	 */ 
 	init_IO();
 	
-	/* Shell interface... */
+	/** 
+ 	 * Shell interface... : shell>
+ 	 * Command : 	- exit
+ 	 * 	 	- infosuper : Information of super block.
+ 	 * 	 	- formatsuper : Set the super block. 
+ 	 * 	 	- infogroup : Information of group descriptor
+ 	 * 	 	- formatgroup : Set the group descriptor
+ 	 **/
 	while(1){
 		printf("shell>");
 		memset(command_buffer,0,256);
 		scanf("%255s",command_buffer);
 		getchar();
-		/*
+		/**
 		 * Now linear search O(n) algorithm
 		 * But It's quite acceptable
 		 */
@@ -29,18 +52,22 @@ int main(){
 			break;
 		}
 		if(strcmp(command_buffer,"infosuper")==0){
-			ku_ext2_info_super();
+			/* super.h :: ku_ext2_info_super() */
+			ku_ext2_info_super(); 
 			continue;
 		}
 		if(strcmp(command_buffer,"formatsuper")==0){
+			/* super.h :: ku_ext2_format_super() */
 			ku_ext2_format_super();
 			continue;
 		}
 		if(strcmp(command_buffer,"infogroup")==0){
+			/* group.h :: ku_ext2_info_group() */
 			ku_ext2_info_group();
 			continue;
 		}
 		if(strcmp(command_buffer,"formatgroup")==0){
+			/* group.h :: ku_ext2_format_group() */
 			ku_ext2_format_group();
 			continue;
 		}
